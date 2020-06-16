dfd300bdba940f2afcea371783515707d152908c
aurelio4 <aurelioarcabascio@gmail.com>
2020-06-16 05:32:12 -0400

    binary search done

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -7,8 +7,25 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    if len(arr) == 0:
+        return -1
+    
+    low = 0
+    high = len(arr) - 1
 
-    # Your code here
+    while low <= high:
+        middle = int((low + high) / 2)
+        guess = arr[middle]
+        if guess == target:
+            return middle
+        if guess > target:
+            high = middle - 1
+        else:
+            low = middle + 1
 
+    return -1
 
-    return -1  # not found
+arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
+arr2 = []
+binary_search(arr1, -8)
+binary_search(arr2, 6)
